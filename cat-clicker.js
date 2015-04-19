8e1f4ce1eef2c835a2d8af2b8cf749f3421e36b2
Omid Hezaveh <ommmmid@gmail.com>
2015-04-19 08:43:59 +0300

    Show number of clicks on each cat seperately

--- cat-clicker.js
+++ cat-clicker.js
@@ -1,16 +1,33 @@
 "use strict"
 
-var counterDisp = document.getElementById('clickcounter');
-var photo = document.getElementById('photo');
+var counterDisp1 = document.getElementById('clickcounter1');
+var photo1 = document.getElementById('catphoto1');
+
+
+var counter1 = 0;
+var counter2 = 0; 
+
+function increment1(){
+    counter1++;
+    counterDisp1.innerHTML = "The number of clicks: " + counter1;
+}
+
+if(photo1) {
+    photo1.addEventListener('click', increment1, false);
+
+} 
+
+var counterDisp2 = document.getElementById('clickcounter2');
+var photo2 = document.getElementById('catphoto2');
 var counter = 0; 
 
 
-function increment(){
-    counter++;
-    counterDisp.innerHTML = "The number of clicks: " + counter;
+function increment2(){
+    counter2++;
+    counterDisp2.innerHTML = "The number of clicks: " + counter2;
 }
 
-if(photo) {
-    photo.addEventListener('click', increment, false);
+if(photo2) {
+    photo2.addEventListener('click', increment2, false);
 
 } 
