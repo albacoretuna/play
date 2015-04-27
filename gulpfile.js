6f4c479dc6dec8a9efd4f3e5999284ca9e4639d9
Omid Hezaveh <a@gik.fi>
2015-04-27 21:04:07 +0300

    Live reload by browsersync enabled

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-gulp.task('default', function() {
+gulp.task('serve', function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+            }
+            });
+    
+    gulp.watch(['*.html', 'scripts/**/*.js'], {cwd: 'app'}, reload);
     
     });
