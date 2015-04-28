2095b49bfed6613bf0a23ea05ceae76499a236ae
Omid Hezaveh <a@gik.fi>
2015-04-28 16:27:11 +0300

    catclicker-premium add ol

--- app/scripts/cat-clicker-premium.js
+++ app/scripts/cat-clicker-premium.js
@@ -3,9 +3,21 @@
 var counterDisp1 = document.getElementById('clickcounter1');
 var photo1 = document.getElementById('catphoto1');
 
+var cats = {
+    name:['Cat One','cat Two','cat three','cat four','cat five'],
+    totalClicks: [0,0,0,0,0],
+    photoUrl:[
+    'https://lh3.ggpht.com/AFZIKl2kWlgLiij6eJRA_utASfg_BjNIcYmXmthEG0MKmpsMoFx2i1TVt3ZwQMZiQmE=w300', 
+    'http://www.catchannel.com/images/older-cat-comfort.jpg',
+    'http://www.catchannel.com/images/british-shorthair-cats-dogs.jpg',
+    'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=1870258',
+    'http://assets.dogtime.com/asset/image/4fa005c186cf65206a0005e6/max_300_cat-at-scratching-post.jpg']};
+
 
 var counter1 = 0;
 var counter2 = 0; 
+
+document.querySelector('#cat-display').innerHTML = "<img src="+cats.photoUrl[0]+" />";
 
 function increment1(){
     counter1++;
